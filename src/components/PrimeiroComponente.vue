d779e20ab12624db24ae8329f40e5b16698c2388
kauaDev22 <eukauapro@gmail.com>
2024-04-13 12:35:47 -0300

    commit teste

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,20 @@
+<template>
+    
+    <h1>Olá Vue!</h1>
+    <p> my name is {{ nome }}</p>
+    <p>My work is a {{ profissao }}</p>
+</template>
+
+<script>
+
+
+export default {
+    name:'PrimeiroComponente',
+    data(){
+        return{
+            nome:"Kauã",
+            profissao: "Dev Front end"
+        }
+    }
+}
+</script>
